6cf67dcb39aab8f396c94761f0248c98da12a604
reggiepardales <reggie.pardales@pointwest.com.ph>
2017-06-02 02:15:40 +0800

    added main.java

---
+++ PeopleLocatorSystemCore/src/com/pointwest/pls/main/main.java
@@ -0,0 +1,9 @@
+package com.pointwest.pls.main;
+
+public class main {
+	
+	public static void main(String args[]) {
+        System.out.println("People Locator System");
+        System.out.println("Welcome!!!");
+    }
+}
