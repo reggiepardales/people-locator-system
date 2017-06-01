484c0bc5fa820a274eed2cf8765855a6cda70457
reggiepardales <reggie.pardales@pointwest.com.ph>
2017-06-02 02:09:28 +0800

    deleted main.java

--- PeopleLocatorSystemCore/src/com/pointwest/pls/main/main.java
+++
@@ -1,9 +0,0 @@
-package com.pointwest.pls.main;
-
-public class main {
-	
-	public static void main(String args[]) {
-        System.out.println("People Locator System");
-        System.out.println("Welcome!!!");
-    }
-}
